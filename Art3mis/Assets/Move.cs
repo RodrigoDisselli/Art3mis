96418140cb4783c32140cc25564946fe8221bc6e
Rodrigo Almeida <disselli.rodrigo@gmail.com>
2019-11-23 15:15:32 -0300

    first commit

---
+++ Art3mis/Assets/Move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    float animationTime;
+
+    void Start()
+    {
+        animationTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animationTime += Time.deltaTime;
+
+        //stellarJump(Vector3.right, 5, 9);
+        lostInSpace(Vector3.forward);
+    }
+
+    private void stellarJump(Vector3 direction, float moveTime, float jump)
+    {
+
+        if (animationTime <= moveTime)
+        {
+            transform.Translate(direction * Time.deltaTime, Space.World);
+        }
+        else if (animationTime > moveTime && animationTime < jump)
+        {
+            transform.Translate(direction * Time.deltaTime / 2, Space.World);
+        }
+        else
+        {
+            transform.Translate(direction * Time.deltaTime * 50, Space.World);
+        }
+    }
+
+    private void lostInSpace(Vector3 direction)
+    {
+        transform.Translate(direction * Time.deltaTime, Space.World);
+        transform.Rotate(new Vector3(45,35,55) * Time.deltaTime/3);
+    }
+
+}
---
+++ Art3mis/Assets/Scenes/laser_grow.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser_grow : MonoBehaviour
+{
+    public float speed = 5f;
+    Vector3 temp;
+
+    /*public bool switchValue = false;
+
+    void upDownScale(bool switchValue)
+    {
+        if(switchValue == false)
+        {
+            temp = transform.localScale;
+            temp.x += 500;
+
+            transform.localScale = temp;
+
+            switchValue = true;
+        }
+        if (switchValue == true)
+        {
+            temp = transform.localScale;
+            temp.x -= 500;
+
+            transform.localScale = temp;
+
+            switchValue = false;
+        }
+    }
+
+    float teste(bool switchValue)
+    {
+
+        if (switchValue == false)
+        {
+            switchValue = true;
+            return 4f;
+        }
+        if (switchValue == true)
+        {
+            switchValue = false;
+            return -4f;
+        }
+
+        return 0;
+    }*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //time += Time.deltaTime;
+
+        temp = transform.localScale;
+        temp.x -= Time.deltaTime;
+
+        transform.localScale = temp;
+
+        
+    }
+}
